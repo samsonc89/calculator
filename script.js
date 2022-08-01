4c57a66d343a8d88a5773886c5f5a271426cffe6
Samson Cheung <samsonc89@yahoo.com>
2022-08-01 13:13:49 -0700

    Add selector for all buttons

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 "use strict";
+const clear = document.querySelector("#btnClear");
+const del = document.querySelector("#btnDel");
+const percent = document.querySelector("#btn%");
+const divide = document.querySelector("#btn/");
+const seven = document.querySelector("#btn7");
+const eight = document.querySelector("#btn8");
+const nine = document.querySelector("#btn9");
+const multiply = document.querySelector("#btnX");
+const four = document.querySelector("#btn4");
+const five = document.querySelector("#btn5");
+const six = document.querySelector("#btn6");
+const subtract = document.querySelector("#btn-");
+const one = document.querySelector("#btn1");
+const two = document.querySelector("#btn2");
+const three = document.querySelector("#btn3");
+const add = document.querySelector("#btn+");
+const zero = document.querySelector("#btn0");
+const negative = document.querySelector("#btn+-");
+const decimal = document.querySelector("#btn.");
+const equals = document.querySelector("#btn=");
 
 function add(a, b) {
   return a + b;

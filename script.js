0addfdcd30d148873b0ed9cb6cc377f3f0b22fa8
Samson Cheung <samsonc89@yahoo.com>
2022-08-01 13:20:18 -0700

    Add selectors for display divs

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
 "use strict";
-const clear = document.querySelector("#btnClear");
-const del = document.querySelector("#btnDel");
-const percent = document.querySelector("#btn%");
-const divide = document.querySelector("#btn/");
-const seven = document.querySelector("#btn7");
-const eight = document.querySelector("#btn8");
-const nine = document.querySelector("#btn9");
-const multiply = document.querySelector("#btnX");
-const four = document.querySelector("#btn4");
-const five = document.querySelector("#btn5");
-const six = document.querySelector("#btn6");
-const subtract = document.querySelector("#btn-");
+//button selectors
 const one = document.querySelector("#btn1");
 const two = document.querySelector("#btn2");
 const three = document.querySelector("#btn3");
-const add = document.querySelector("#btn+");
+const four = document.querySelector("#btn4");
+const five = document.querySelector("#btn5");
+const six = document.querySelector("#btn6");
+const seven = document.querySelector("#btn7");
+const eight = document.querySelector("#btn8");
+const nine = document.querySelector("#btn9");
 const zero = document.querySelector("#btn0");
+const subtract = document.querySelector("#btn-");
+const add = document.querySelector("#btn+");
+const percent = document.querySelector("#btn%");
+const divide = document.querySelector("#btn/");
+const multiply = document.querySelector("#btnX");
 const negative = document.querySelector("#btn+-");
 const decimal = document.querySelector("#btn.");
 const equals = document.querySelector("#btn=");
+const clear = document.querySelector("#btnClear");
+const del = document.querySelector("#btnDel");
 
+//display selectors
+const current = document.querySelector("#current");
+const total = document.querySelector("#total");
+
+//math operator functions
 function add(a, b) {
   return a + b;
 }
